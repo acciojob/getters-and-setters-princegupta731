b58b18c4229d8e5dcc7d13ac9ec63cf12d5ac167
Prince gupta <pg0799231@gmail.com>
2025-11-12 16:42:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
 //complete this code
-class Person {}
+class Person {
+	constructer(name, age){
+		this._name = name;
+		this._age = age;
+	}
+	get name(){
+		return this._name;
+	}
+	get age(){
+		return this._age;
+	}
+	set age(newAge){
+		return this._newAge;
+	}
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		console.log(`${this.name} is studying`);
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		console.log(`${this.name} is teaching`);
+	}
+}
+
+// exemapl
+const person = new Person("prince", 22);
+console.log(new.person);
+
+const age = 22;
+console.log(person.age);
+
+const student = new Student("Alice", 30);
+student.study();
+
+const teacher =  new Teacher("rahul", 40);
+teacher.teach();
+
+
 
 // Do not change the code below this line
 window.Person = Person;
